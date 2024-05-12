aed8aa657dd16809f1f945a928d0bfdd47779527
satyajeet <satya126812@gmail.com>
2024-05-12 10:57:32 +0530

    start point :

---
+++ escapegame.py
@@ -0,0 +1,11 @@
+print("welcome to a horror escape game made by satyajeet kar ")
+intro="""Welcome to 'Echoes of Fear: The Haunted Mansion' – a spine-chilling escape game that will test your courage and intellect to the limit. Step into the shoes of a brave soul trapped within the dark confines of an abandoned mansion, where every corner holds secrets, and every step could be your last.
+
+As the doors slam shut behind you, an overwhelming sense of dread envelops you. You are not alone. Malevolent spirits roam these halls, their whispers echoing through the darkness. To survive, you must unravel the mysteries that shroud this haunted mansion and find a way to escape its sinister clutches.
+
+Prepare to navigate through twisted corridors, solve fiendish puzzles, and confront your deepest fears as you journey deeper into the heart of darkness. But beware – the spirits that haunt this place will stop at nothing to claim your soul.
+
+Dare you face the echoes of fear that reverberate through this accursed mansion? Your journey into terror begins now."""
+print(intro)
+print("if you want to enter the house you first find the key")
+  
---
+++ main.py
@@ -0,0 +1,67 @@
+print("Welcome to 'ChronoQuiz: Challenge of the Ages' – the ultimate console game blending general knowledge with current affairs! Embark on an exhilarating journey through time and trivia as you test your intellect against the clock. From ancient history to cutting-edge news, every question challenges your wits and keeps you on the edge of your seat. Are you ready to become a master of both past and present? Get ready to unlock the mysteries of time and conquer the quiz of the ages!")
+answer=False
+correctanswer=0
+q1="""Which country hosted the 2024 Summer Olympics?
+A) United States
+B) France
+C) Japan
+D) Australia"""
+print(q1)
+answer=input()
+if"b" in answer:
+    print("correct")
+    correctanswer+=1
+else:
+    print("wrong")
+q1="""Who won the Nobel Peace Prize in 2024 for their efforts in climate change activism?
+A) Greta Thunberg
+B) Malala Yousafzai
+C) Al Gore
+D) Kailash Satyarthi"""
+print(q1)
+answer=input()
+if"a" in answer:
+    print("correct")
+    correctanswer+=1
+else:
+    print("wrong")
+q1="""Which country recently launched its first mission to Mars?
+A) India
+B) China
+C) United Arab Emirates
+D) Russia"""
+print(q1)
+answer=input()
+if"c" in answer:
+    print("correct")
+    correctanswer+=1
+else:
+    print("wrong")
+q1="""Which global organization celebrated its 75th anniversary in 2024?
+A) United Nations
+B) World Health Organization
+C) International Monetary Fund
+D) World Trade Organization"""
+print(q1)
+answer=input()
+if"a" in answer:
+    print("correct")
+    correctanswer+=1
+else:
+    print("wrong")
+q1="""Who became the first woman to climb Mount Everest without supplemental oxygen in 2024?
+A) Junko Tabei
+B) Pasang Lhamu Sherpa
+C) Reinhold Messner
+D) Nirmal Purja"""
+print(q1)
+answer=input()
+if"b" in answer:
+    print("correct")
+    correctanswer+=1
+else:
+    print("wrong")
+
+print(f"you give {correctanswer} correct answer")
+
+
